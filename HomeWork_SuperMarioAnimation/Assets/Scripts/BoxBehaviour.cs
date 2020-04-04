6624b468c1922305073db392bc185dfec029078d
AstrexXx <astrex17@abv.bg>
2020-04-04 03:52:01 +0300

    Night Update

---
+++ HomeWork_SuperMarioAnimation/Assets/Scripts/BoxBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject contains;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && !contains.GetComponent<Animator>().GetBool("onSpawn"))
+        {
+            Debug.Log("Player Hit the box and spawned its contains.");
+            
+            // Spawn:
+            contains.GetComponent<Animator>().SetBool("onSpawn", true);
+        }
+    }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Mushroom"))
+        {
+            Debug.Log("Mushroom Exit!");
+        }
+    }
+}
---
+++ HomeWork_SuperMarioAnimation/Assets/Scripts/BoxContainsBehaviour.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxContainsBehaviour : MonoBehaviour
+{
+    private new Rigidbody2D rigidbody;
+    
+}
